6df23b48c397f7a5f816c12e83d247a6d5313fd3
Jonathan <dasc@Mac-mini-de-DASC.local>
2025-02-13 07:22:04 -0700

    mi primera ventana en Java

---
+++ src/aplication/Ventana.java
@@ -0,0 +1,35 @@
+package aplication;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ventana extends JFrame{
+
+	public Ventana(String title) {
+		
+		this.setTitle(title);
+		this.setVisible(true);
+		this.setSize(500, 500);
+		
+		this.setResizable(true);
+		this.setLayout(null);
+		
+		//que_pasa_cuando_cierro_mi_aplicación
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//sobre_que_elemento_colocaré_el_item
+		this.setLocationRelativeTo(null);
+		
+		JLabel etiqueta1 = new JLabel("Bienvenido");
+		
+		etiqueta1.setSize(100, 100);
+		etiqueta1.setLocation(10, 50);
+		etiqueta1.setBackground(Color.ORANGE);
+		
+		this.add(etiqueta1);
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module IDSTM {
+	requires java.desktop;
+}
