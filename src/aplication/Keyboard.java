ae9473f3ba826dd98ec66110362ed3645d2bca37
Jonathan Soto <jonathan@Jonathans-MacBook-Air.local>
2025-04-23 07:31:55 -0700

    imlpementacion fuente y teclado 3

--- src/aplication/Keyboard.java
+++ src/aplication/Keyboard.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import aplication.Paint.Figura;
 import aplication.Paint.MyPoint;
@@ -13,12 +14,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.awt.event.ActionListener;
@@ -26,9 +34,13 @@ import java.awt.event.ActionEvent;
 
 public class Keyboard implements KeyListener{
 
-	private JFrame frame;
-	private int x=200,y=200;
+	private JFrame frame; 
+	private Player player,shadow;
 	public PaintPanel panel_2;
+	Timer timer,timer2;
+	private int seg = 0, lastPress = 0;
+
+	private ArrayList<Player> obstaculos = new ArrayList<Player>();
 
 	/**
 	 * Launch the application.
@@ -61,15 +73,33 @@ public class Keyboard implements KeyListener{
 		frame.setBounds(100, 100, 515, 596);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		usingCustomFonts();
+		
+		player = new Player(200,200,25,25,Color.green);
+		shadow = new Player(200,200,25,25,Color.green);
+		
+		obstaculos.add(new Player(150, 350, 220, 35,Color.orange));
+		obstaculos.add(new Player(150, 80, 220, 35,Color.orange));
+		//obstaculos.add(new Player(50, 100, 20, 220,Color.orange));
+		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.SOUTH);
 		
 		JButton btnNewButton = new JButton("Reiniciar");
+		btnNewButton.setFont(new Font("Boost Player PERSONAL USE ONLY", Font.PLAIN, 20));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				x =200;
-				y = 200;
+				player.x = 200;
+				player.y = 200;
+				
+				shadow.x = 200;
+				shadow.y = 200;
+				
+				lastPress = 0;
+				
+				timer.stop();
+				
 				panel_2.repaint();
 				panel_2.setFocusable(true);
 				panel_2.requestFocus();
@@ -82,6 +112,7 @@ public class Keyboard implements KeyListener{
 		
 		JLabel lblNewLabel = new JLabel("0:00");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		//lblNewLabel.setFont(new Font("Boost Player PERSONAL USE ONLY", Font.PLAIN, 20));
 		panel_1.add(lblNewLabel);
 		
 		panel_2 = new PaintPanel();
@@ -91,6 +122,57 @@ public class Keyboard implements KeyListener{
 		
 		panel_2.addKeyListener(this);
 		panel_2.setFocusable(true);
+		
+		ActionListener taskPerformer = new ActionListener() {
+		       
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub 
+				
+				String [] split_string = lblNewLabel.getText().split(":");  
+				int mil = Integer.parseInt(split_string[1]);
+				
+				mil+=1;
+				
+				if(mil>=10) {
+					seg++;
+					mil = 1;
+				} 
+				
+				lblNewLabel.setText(seg+":"+mil+""); 
+				
+			}
+		  };
+		  timer = new Timer(100, taskPerformer);
+		  
+		  ActionListener movimiento = new ActionListener() {
+		       
+				@Override
+				public void actionPerformed(ActionEvent e) { 
+					update();
+				}
+		   };
+		   timer2 = new Timer(4, movimiento);
+	}
+	
+	public void usingCustomFonts() {
+	    GraphicsEnvironment GE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	    List<String> AVAILABLE_FONT_FAMILY_NAMES = Arrays.asList(GE.getAvailableFontFamilyNames());
+	    try {
+	        List<File> LIST = Arrays.asList(
+	          new File("BoostPlayer_PERSONAL_USE_ONLY.otf")
+	        );
+	        for (File LIST_ITEM : LIST) {
+	            if (LIST_ITEM.exists()) {
+	                Font FONT = Font.createFont(Font.TRUETYPE_FONT, LIST_ITEM);
+	                if (!AVAILABLE_FONT_FAMILY_NAMES.contains(FONT.getFontName())) {
+	                    GE.registerFont(FONT);
+	                }
+	            }
+	        }
+	    } catch (FontFormatException | IOException exception) {
+	        JOptionPane.showMessageDialog(null, exception.getMessage());
+	    }
 	}
 	
 	class PaintPanel extends JPanel{
@@ -106,8 +188,18 @@ public class Keyboard implements KeyListener{
 	       
 	       Graphics2D g2 = (Graphics2D) g; 
 	       
-	       g2.setColor(Color.green);  
-	       g2.fillRect(x,y,30,30);
+	       g2.setColor(player.c);  
+	       g2.fillRect(player.x,player.y,player.w,player.h);
+	       
+	       
+	       for (Player pared : obstaculos) {
+	    	   
+	    	   g2.setColor(pared.c);
+		       g2.fillRect(pared.x, pared.y, pared.w, pared.h);
+		        
+	       }
+	       
+	       
 	        
 	   }
 		
@@ -122,17 +214,76 @@ public class Keyboard implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getKeyCode() == 87) {
-			y-=5;
+		
+		timer.start(); 
+		timer2.start();
+		
+		lastPress = e.getKeyCode();
+		
+		update();
+		
+		//panel_2.repaint();
+	}
+	
+	public void update()
+	{
+		Boolean m = true;
+		for (Player pared : obstaculos) {
+			if(player.colision(pared)) {
+				m = false;
+			}
 		}
-		if(e.getKeyCode() == 83) {
-			y+=5;
+		
+		if(m) {
+			shadow.x = player.x;
+			shadow.y = player.y;
 		}
-		if(e.getKeyCode() == 65) {
-			x-=5;
+		
+		if(lastPress == 87) { 
+			
+			if(m)
+				player.y-=1;
+			else {
+				player.y = shadow.y;
+				player.x = shadow.x; 
+				
+				lastPress = 0;
+				
+			} 
 		}
-		if(e.getKeyCode() == 68) {
-			x+=5;
+		if(lastPress == 83) {
+			
+			if(m)
+				player.y+=1;
+			else {
+				player.y = shadow.y;
+				player.x = shadow.x;
+				lastPress = 0;
+			}
+			 
+			
+		}
+		if(lastPress == 65) {
+			
+			if(m)
+				player.x-=1;
+			else {
+				player.y = shadow.y;
+				player.x = shadow.x;
+				lastPress = 0; 
+			}
+			 
+		}
+		if(lastPress == 68) {
+			
+			if(m)
+				player.x+=1;
+			else {
+				player.y = shadow.y;
+				player.x = shadow.x;
+				lastPress = 0;
+			}
+			 
 		}
 		
 		panel_2.repaint();
@@ -144,12 +295,34 @@ public class Keyboard implements KeyListener{
 		
 	}
 
+	class Player {
+		
+		int x=0,y=0,w=0,h=0;
+		Color c= Color.black;
+		
+		public Player(int x, int y, int w, int h, Color c) {
+			
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+			this.c = c;
+		}
+		
+		public Boolean colision(Player target) { 
+			
+			if(this.x < target.x + target.w &&
+	                this.x + this.w > target.x &&
+	                this.y < target.y + target.h &&
+	                this.y + this.h > target.y) {
+				
+				
+				return true;
+			}
+			return false;
+			
+		}
+	}
 	
-	/*
-	 * return (this.x < target.x + target.width &&
-                this.x + this.width > target.x &&
-                this.y < target.y + target.height &&
-                this.y + this.height > target.y);
-	 */
 
 }
